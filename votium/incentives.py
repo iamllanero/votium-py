c76d9eb50a20f78b99792089403aed954ca4eadd
iamllanero <iamllanero@gmail.com>
2023-11-04 08:20:55 -0500

    Revamp snapshot and incentives

--- votium/incentives.py
+++ votium/incentives.py
@@ -4,16 +4,21 @@ from web3 import Web3
 import csv
 import json
 import os
-import snapshot
-import votium.rounds as rounds
+from snapshot import get_proposal
+from votium.rounds import get_last_round, get_last_completed_round
+from votium.events import get_events
 
 load_dotenv()
 WEB3_HTTP_PROVIDER = os.environ.get("WEB3_HTTP_PROVIDER")
 w3 = Web3(Web3.HTTPProvider(WEB3_HTTP_PROVIDER))
 
+SNAPSHOT_PROPOSALS_FILE = "output/snapshot/proposals.csv"
+MAPPED_PROPOSALS_FILE = "output/incentives/proposals_mapped.csv"
+
 VOTIUM1_ADDRESS = '0x19BBC3463Dd8d07f55438014b021Fb457EBD4595'
 VOTIUM1_ABI = 'data/abis/votium1.json'
 
+# Votium v2 starts round 53 (Sept 13, 2023)
 VOTIUM2_ADDRESS = '0x63942E31E98f1833A234077f47880A66136a2D1e'
 VOTIUM2_ABI = 'data/abis/votium2.json'
 
@@ -24,7 +29,18 @@ GAUGES = 'data/gauges.json'
 OUTPUT_DIR = "output/incentives"
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
+VOTIUM1_INCENTIVES = f"{OUTPUT_DIR}/votium1_incentives.csv"
 VOTIUM2_INCENTIVES = f"{OUTPUT_DIR}/votium2_incentives.csv"
+
+TOKEN_MAP = {}
+BLOCK_TIME_MAP = {}
+GAUGE_MAP = {}
+
+with open(GAUGES) as f:
+    json = json.load(f)
+    for key, value in json["gauges"].items():
+        GAUGE_MAP[key] = value["shortName"]
+
 
 def load_contract(abi, address):
     with open(abi) as f:
@@ -33,8 +49,63 @@ def load_contract(abi, address):
     return contract
 
 
+def get_mapped_proposals():
+    """Map the proposal_id from Snapshot to the Votium Event proposal_id."""
+
+    if os.path.exists(MAPPED_PROPOSALS_FILE):
+        with open(MAPPED_PROPOSALS_FILE, "r") as f:
+            reader = csv.reader(f)
+            next(reader)
+            proposals = list(reader)
+        return proposals
+    
+    proposals = [[
+        "round", 
+        "start", 
+        "end", 
+        "id", 
+        "title",
+        "keccak_id",
+    ]]
+
+    with open(SNAPSHOT_PROPOSALS_FILE) as f:
+        reader = csv.reader(f)
+        next(reader)
+        proposals = list(reader)
+    
+    mapped_proposals = []
+    for proposal in proposals:
+        proposal_id = proposal[3]
+        if proposal_id.startswith("0x"):
+            keccak_id = w3.keccak(hexstr=proposal_id)
+        else:
+            keccak_id = w3.keccak(text=proposal_id)
+        mapped_proposals.append([
+            proposal[0],
+            proposal[1],
+            proposal[2],
+            proposal[3],
+            proposal[4],
+            keccak_id.hex(),
+        ])
+
+    with open(MAPPED_PROPOSALS_FILE, "w") as f:
+        writer = csv.writer(f)
+        writer.writerow([
+            "round", 
+            "start", 
+            "end", 
+            "id", 
+            "title",
+            "keccak_id",
+        ])
+        writer.writerows(mapped_proposals)
+    
+    return mapped_proposals
+
+
 def get_incentive_v1(round, start_block, end_block):
-    proposal = snapshot.get_proposal(round)
+    proposal = get_proposal(round)
     votium = load_contract(VOTIUM1_ABI, VOTIUM1_ADDRESS)
     event_filter = votium.events.Bribed().create_filter(
         fromBlock=start_block, toBlock=end_block)
@@ -143,7 +214,7 @@ def get_incentive(round):
     # Check for existing file
     file_path = f"{OUTPUT_DIR}/round_{round}_incentives.csv"
     if os.path.exists(file_path) and \
-        round <= rounds.get_last_completed_round():
+        round <= get_last_completed_round():
         with open(file_path, "r") as f:
             f.readline() # Skip header
             reader = csv.reader(f)
@@ -151,12 +222,12 @@ def get_incentive(round):
         return incentives
 
     # Get start block
-    proposal = snapshot.get_proposal(round)
+    proposal = get_proposal(round)
     start_block = proposal["data"]["proposal"]["snapshot"]
 
     # Get end block
-    if round < rounds.get_last_round():
-        proposal = snapshot.get_proposal(round+1)
+    if round < get_last_round():
+        proposal = get_proposal(round+1)
         end_block = proposal["data"]["proposal"]["snapshot"]
     else:
         end_block = w3.eth.block_number
@@ -189,8 +260,80 @@ def get_incentive(round):
     return incentives
 
 
-def get_incentive_events():
-    """Get the NewIncentive events for all rounds."""
+def get_all_incentive_events_v1():
+    """Get all the Bribed events."""
+
+    incentives = []
+    start_block = 13262700 # First block with Bribed event
+    end_block = 18043767 - 1 # One less starting block for Votium v2
+    # end_block = start_block + 100000
+
+    # Check if file exists and if so get the highest number block
+    if os.path.exists(VOTIUM1_INCENTIVES):
+        with open(VOTIUM1_INCENTIVES, "r") as f:
+            reader = csv.reader(f)
+            next(reader)
+            incentives = list(reader)
+            start_block = max(int(row[-1]) for row in incentives) + 1
+
+    with open(VOTIUM1_ABI) as f:
+        abi = f.read()
+    contract = w3.eth.contract(address=VOTIUM1_ADDRESS, abi=abi)
+
+    print(f"Collecting Bribed events from {start_block} to {end_block}")
+    event_filter = contract.events.Bribed().create_filter(
+        fromBlock=start_block, toBlock=end_block)
+    event_log = event_filter.get_all_entries()
+
+    print(f"Writing {len(event_log)} events to {VOTIUM1_INCENTIVES}")
+    for event in event_log:
+        # AttributeDict({'args': AttributeDict({'_proposal': b'\xc8A\xdb\x89*X\x16\x8d!&.\xb8\xe2\xf9}e\x1f\xb3T\x89o\xa9\n\xc3\xd6\xbc\xfa\xd0\x03\x19\xc55',
+        #     '_token': '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+        #     '_amount': 9600000000000000000000,
+        #     '_choiceIndex': 22}),
+        #     'event': 'Bribed',
+        #     'logIndex': 548,
+        #     'transactionIndex': 319,
+        #     'transactionHash': HexBytes('0x11d8023e4a99a850f2fbd3727b104d075c9af99458798fa7ae29d6b92882b8c9'),
+        #     'address': '0x19BBC3463Dd8d07f55438014b021Fb457EBD4595',
+        #     'blockHash': HexBytes('0xc7c4a936dbda35c108faa7d66389a483407aef156e5f5e46977c267044c76dbd'),
+        #     'blockNumber': 13262700}),
+        incentives.append([
+            event["args"]["_proposal"].hex(),
+            event["args"]["_token"],
+            event["args"]["_amount"],
+            event["args"]["_choiceIndex"],
+            event["event"],
+            event["logIndex"],
+            event["transactionIndex"],
+            event["transactionHash"].hex(),
+            event["address"],
+            event["blockHash"].hex(),
+            event["blockNumber"],
+        ])
+
+    with open(VOTIUM1_INCENTIVES, "w") as f:
+        writer = csv.writer(f)
+        writer.writerow([
+            '_proposal',
+            '_token',
+            '_amount',
+            '_choiceIndex',
+            'event',
+            'logIndex',
+            'transactionIndex',
+            'transactionHash',
+            'address',
+            'blockHash',
+            'blockNumber',
+        ])
+        writer.writerows(incentives)
+
+    return incentives
+
+
+def get_all_incentive_events_v2():
+    """Get all the NewIncentive events."""
 
     incentives = []
     start_block = 18043767 # Starting block for Votium v2
@@ -258,16 +401,195 @@ def get_incentive_events():
     return incentives
 
 
+def get_token(token_address):
+    """Get the token name and symbol from the token address."""
+
+    if token_address in TOKEN_MAP:
+        return TOKEN_MAP[token_address]
+    else:
+        erc20_contract = load_contract(ERC20_ABI, token_address)
+        token_symbol = erc20_contract.functions.symbol().call()
+        token_name = erc20_contract.functions.name().call()
+        TOKEN_MAP[token_address] = [token_symbol, token_name]
+
+        return token_symbol, token_name
+
+
+def get_block_time(block_number):
+    """Get the timestamp for a given block number."""
+
+    if block_number in BLOCK_TIME_MAP:
+        return BLOCK_TIME_MAP[block_number]
+    else:
+        timestamp = w3.eth.get_block(block_number)["timestamp"]
+        BLOCK_TIME_MAP[block_number] = timestamp
+        return timestamp
+    
+
+def get_incentives_v1(mapped_proposals, initiated, bribed):
+    with alive_bar(52) as bar:
+        for round in range(1,53):
+            bar()
+            bar.text(f"Round {round}")
+
+            # Check if output already exists
+            file_path = f"{OUTPUT_DIR}/round_{round}_incentives.csv"
+            if os.path.exists(file_path):
+                continue
+
+            proposal = get_proposal(round)
+            proposal_id = [p for p in mapped_proposals if p[0] == str(round)][0][5]
+            events = [b for b in bribed if b[0] == proposal_id[2:]]
+            incentives = []
+            for e in events:
+                choice_index = int(e[3]) - 1
+                gauge = proposal[choice_index][0]
+                token_address = e[1]
+                token_symbol, token_name = get_token(token_address)
+                amount = e[2]
+                transaction_hash = e[7]
+                block_hash = e[9]
+                block_number = e[10]
+                timestamp = get_block_time(block_number)
+                score = proposal[choice_index][2]
+                incentives.append([
+                    gauge,
+                    amount,
+                    token_symbol,
+                    timestamp,
+                    token_address,
+                    token_name,
+                    transaction_hash,
+                    block_hash,
+                    block_number,
+                    score,
+                ])
+            
+            # Save as CSV
+            with open(file_path, "w") as f:
+                writer = csv.writer(f)
+                writer.writerow([
+                    "gauge", 
+                    "amount", 
+                    "token_symbol", 
+                    "timestamp",
+                    "token_address",
+                    "token_name",
+                    "transaction_hash",
+                    "block_hash", 
+                    "block_number", 
+                    "score", 
+                ])
+                writer.writerows(incentives)
+
+
+def get_gauge_score(proposal, gauge_address):
+    gauge_name = GAUGE_MAP[gauge_address]
+    for choice in proposal:
+        if choice[0] == gauge_name:
+            return gauge_name, choice[2]
+
+
+def get_incentives_v2(new_incentives):
+    with alive_bar(int(get_last_round()) - 52) as bar:
+        for round in range(53, get_last_round() + 1):
+            bar()
+            print(f"Round {round}")
+            bar.text(f"Round {round}")
+
+            # Check if output already exists
+            file_path = f"{OUTPUT_DIR}/round_{round}_incentives.csv"
+            if os.path.exists(file_path):
+                continue
+
+            proposal = get_proposal(round)
+            incentives = []
+            events = [e for e in new_incentives if e[0] == str(round)]
+            for e in events:
+                gauge_address = e[1]
+                (gauge, score) = get_gauge_score(proposal, gauge_address)
+                token_address = e[4]
+                token_symbol, token_name = get_token(token_address)
+                amount = e[5]
+                transaction_hash = e[12]
+                block_hash = e[14]
+                block_number = e[15]
+                timestamp = get_block_time(block_number)
+                incentives.append([
+                    gauge,
+                    amount,
+                    token_symbol,
+                    timestamp,
+                    token_address,
+                    token_name,
+                    transaction_hash,
+                    block_hash,
+                    block_number,
+                    score,
+                ])
+
+            # Save as CSV
+            with open(file_path, "w") as f:
+                writer = csv.writer(f)
+                writer.writerow([
+                    "gauge", 
+                    "amount", 
+                    "token_symbol", 
+                    "timestamp",
+                    "token_address",
+                    "token_name",
+                    "transaction_hash",
+                    "block_hash", 
+                    "block_number", 
+                    "score", 
+                ])
+                writer.writerows(incentives)
+
+
+
 def main():
     """Get the incentives for all rounds."""
 
-    get_incentive_events()
+    with alive_bar(4) as bar:
 
-    with alive_bar(rounds.get_last_round()) as bar:
-        for round in range(1, rounds.get_last_round()+1):
-            bar()
-            bar.text(f"Round {round}")
-            incentives = get_incentive(round)
+        bar()
+        print("Mapping all Snapshot proposal IDs to Event proposal IDs")
+        mapped_proposals = get_mapped_proposals()
+
+        bar()
+        print("Getting all Initiated events from Votium v1")
+        initiated = get_events(
+            VOTIUM1_ABI,
+            VOTIUM1_ADDRESS,
+            "Initiated",
+            start_block=13209937, # Contract deployed at 13209937
+            end_block=18043767-1 # One less starting block for Votium v2
+        )
+
+        bar()
+        print("Getting all Bribed events from Votium v1")
+        bribed = get_events(
+            VOTIUM1_ABI,
+            VOTIUM1_ADDRESS,
+            "Bribed",
+            start_block=13209937, # Contract deployed at 13209937
+            end_block=18043767-1 # One less starting block for Votium v2
+        )
+
+        bar()
+        print("Getting all NewIncentive events from Votium v2")
+        new_incentives = get_events(
+            VOTIUM2_ABI,
+            VOTIUM2_ADDRESS,
+            "NewIncentive",
+            start_block=18043767, # Starting block for Votium v2
+            end_block=w3.eth.block_number
+        )
+
+
+    get_incentives_v1(mapped_proposals, initiated, bribed)
+
+    get_incentives_v2(new_incentives)
 
 
 if __name__ == "__main__":
--- votium/snapshot.py
+++ votium/snapshot.py
@@ -7,14 +7,10 @@ import os
 import requests
 
 
-OUTPUT_DIR = "output/snapshot/"
-CACHE_DIR = "cache/snapshot/"
-
-
-# Make necessary directories
+OUTPUT_DIR = "output/snapshot"
+CACHE_DIR = "cache/snapshot"
 for dir in [OUTPUT_DIR, CACHE_DIR]:
-    if not os.path.exists(dir):
-        os.makedirs(dir)
+    os.makedirs(dir, exist_ok=True)
 
 
 def _snapshot_graphql(query):
@@ -33,70 +29,71 @@ def _snapshot_graphql(query):
 
 def get_proposal_list():
     """
-    Gets the list of the gauge weight proposals. 
-    Generally should use the cached get_proposals().
+    Gets the list of the gauge weight proposals (incl. test proposals)
     """
 
     # Check cache
-    cache_file = CACHE_DIR + "proposal_list.csv"
+    cache_file = f"{CACHE_DIR}/proposals.json"
     if os.path.exists(cache_file):
-        with open(CACHE_DIR + "proposal_list.csv", "r") as f:
-            f.readline() # Skip header
-            reader = csv.reader(f)
-            proposal_list = list(reader)
+        with open(cache_file, "r") as f:
+            response = json.load(f)
 
-        # Check if current round is in the list
-
-
-        return proposal_list
-
-    # Fetch the gauge weight proposals
-    print(f"Fetching gauge weight proposals...")
-    response = _snapshot_graphql('''
-        query {
-            proposals (
-                first: 100,
-                skip: 0,
-                where: {
-                    space_in: ["cvx.eth"],
-                    title_contains: "gauge weight for"
-                },
-                orderBy: "created",
-                orderDirection: desc
-            ) {
-                id
-                title
-                start
-                end
-                author
+    else:
+        # Fetch the gauge weight proposals
+        print(f"Fetching gauge weight proposals...")
+        response = _snapshot_graphql('''
+            query {
+                proposals (
+                    first: 100,
+                    skip: 0,
+                    where: {
+                        space_in: ["cvx.eth"],
+                        title_contains: "gauge weight for"
+                    },
+                    orderBy: "created",
+                    orderDirection: asc
+                ) {
+                    id
+                    title
+                    start
+                    end
+                    author
+                }
             }
-        }
-        ''')
-    # Throw out the ones with "(TEST)" in the title
-    filtered_response = [proposal for proposal in response['data']
-                         ['proposals'] if "(TEST)" not in proposal['title']]
+            ''')
+
+    data = response['data']['proposals']
 
     proposal_list = []
-    round = len(filtered_response)
-    for proposal in filtered_response:
-        id = proposal['id']
+    round = 0
+    for proposal in data:
+        test = False
         title = proposal['title']
+        if title.startswith("(TEST)"):
+            test = True
+        else:
+            round += 1
+        id = proposal['id']
         start = datetime.datetime.fromtimestamp(proposal['start'])
         end = datetime.datetime.fromtimestamp(proposal['end'])
         proposal_list.append([
-            round,
+            round if not test else '0',
             start,
             end,
             id,
             title
             ])
-        round -= 1
     
     # Sort proposal_list by round
-    proposal_list.sort(key=lambda x: x[0])
+    # proposal_list.sort(key=lambda x: x[0])
 
-    # Save to cache
+    # Save response to cache
     with open(cache_file, "w") as f:
+        json.dump(response, f, indent=4)
+
+    # Save to output
+    output_file = f"{OUTPUT_DIR}/proposals.csv"
+    with open(output_file, "w") as f:
         writer = csv.writer(f)
         writer.writerow(["round", "start", "end", "id", "title"])
         writer.writerows(proposal_list)
@@ -108,154 +105,78 @@ def get_proposal(round):
     """Gets the details of a proposal"""
 
     # Check cache
-    cache_file = CACHE_DIR + f"round_{round}_proposal.json"
+    cache_file = f"{CACHE_DIR}/round_{round}_proposal.json"
     if os.path.exists(cache_file):
         with open(cache_file, "r") as f:
-            proposal = json.load(f)
-        return proposal
+            response = json.load(f)
 
-    # Look up the proposal id
-    proposal_list = get_proposal_list()
-    id = proposal_list[round-1][3]
+    else:
 
-    print(f"Fetching proposal {round} - {id}...")
-    response = _snapshot_graphql(f'''
-        query {{
-            proposal(id:"{id}") {{
-                id
-                title
-                start
-                end
-                snapshot
-                state
-                author
-                created
-                space {{
+        # Look up the proposal id
+        proposal_list = get_proposal_list()
+        # Filter out any with round 0
+        proposal_list = [x for x in proposal_list if x[0] != '0']
+        id = proposal_list[round-1][3]
+
+        print(f"Fetching proposal {round} - {id}...")
+        response = _snapshot_graphql(f'''
+            query {{
+                proposal(id:"{id}") {{
                     id
-                    name
+                    title
+                    body
+                    start
+                    end
+                    snapshot
+                    state
+                    author
+                    created
+                    space {{
+                        id
+                        name
+                    }}
+                    choices
+                    scores
+                    scores_state
+                    scores_total
+                    scores_updated
                 }}
-                choices
             }}
-        }}
-        ''')
-    
-    # Cache the proposal
-    with open(cache_file, "w") as f:
-        json.dump(response, f, indent=4)
-
-    return response["data"]["proposal"]
-
-
-def get_votes(round):
-    """
-    Actual URL fetcher for getting the votes for a proposal.
-    Generally should use the cached get_votes(proposal_id).
-    """
-
-    # Check cache
-    cache_file = CACHE_DIR + f"round_{round}_votes.json"
-    if os.path.exists(cache_file):
-        with open(cache_file, "r") as f:
-            votes = json.load(f)
-        return votes
-
-    # Look up the proposal id
-    proposal_list = get_proposal_list()
-    id = proposal_list[round-1][3]
-
-    # Get the votes
-    print(f"Fetching votes {round} - {id}...")
-    response = _snapshot_graphql(f'''
-    query {{
-        votes (
-            first: 1000
-            skip: 0
-            where: {{
-            proposal: "{id}"
-            }}
-            orderBy: "created",
-            orderDirection: desc
-        ) {{
-            id
-            voter
-            vp
-            vp_state
-            created
-            proposal {{
-                id
-            }}
-            space {{
-                id
-            }}
-            choice
-        }}
-        }}
-    ''')
-    votes = response["data"]["votes"]
-
-    vote_choices = {}
-
-    for vote in votes:
-        vp = vote["vp"]
-        choices = vote["choice"]
-        choice_total = sum(choices.values())
-        for key, value in choices.items():
-            if key not in vote_choices:
-                vote_choices[key] = 0
-            vote_choices[key] += (value / choice_total) * vp
-
-    # Cache the votes if the round is complete
-    if round <= rounds.get_last_completed_round():
+            ''')
+        
+        # Cache the proposal
         with open(cache_file, "w") as f:
-            json.dump(vote_choices, f, indent=4)
+            json.dump(response, f, indent=4)
 
-    return vote_choices
+    choices = response["data"]["proposal"]["choices"]
+    scores = response["data"]["proposal"]["scores"]
 
+    proposal = []
+    for i in range(0, len(choices)):
+        proposal.append([choices[i], i, scores[i], (scores[i]/response["data"]["proposal"]["scores_total"])])
 
-def get_snapshot(round):
+    # Save to output
+    output_file = f"{OUTPUT_DIR}/round_{round}_proposal.csv"
+    with open(output_file, "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(["choice_name", "choice_index", "score", "pct_score"])
+        writer.writerows(proposal)
 
-    # Get proposal
-    proposal = get_proposal(round)
+    return proposal
 
-    # Get votes
-    votes = get_votes(round)
-
-
-    # Sum of all votes
-    total_votes = sum(votes.values())
-
-    # Cross-reference the vote keys with the choice index
-    choice_votes = []
-    for key, vote_count in votes.items():
-        index = int(key) - 1  # Assuming the keys are 1-based
-        if 0 <= index < len(proposal["data"]["proposal"]["choices"]):
-            choice_name = proposal["data"]["proposal"]["choices"][index]
-            choice_votes.append((choice_name, index, vote_count, (vote_count/total_votes)))
-        else:
-            print(f"ERROR: Invalid choice index {index} for {key}")
-
-    # Write to CSV
-    file_path = OUTPUT_DIR + f"round_{round}_snapshot.csv"
-    with open(file_path, 'w', newline='') as csvfile:
-        csv_writer = csv.writer(csvfile)
-        csv_writer.writerow(["choice_name", "choice_index", "votes", "pct_votes"])  # headers
-        for row in choice_votes:
-            csv_writer.writerow(row)
-
-    return choice_votes
 
 def main():
     """Build all snapshots"""
-    with alive_bar(rounds.get_last_round()+1) as bar:
-        bar.text("Proposal list")
+    with alive_bar(1) as bar:
+        print("Getting proposal list from Snapshot")
         bar()
         get_proposal_list()
+
+    with alive_bar(rounds.get_last_round()) as bar:
         for round in range(1, rounds.get_last_round()+1):
             bar.text(f"Round {round}")
             bar()
-            # proposal = get_proposal(round)
-            # votes = get_votes(round)
-            snapshot = get_snapshot(round)
+            proposal = get_proposal(round)
 
 
 if __name__ == "__main__":
